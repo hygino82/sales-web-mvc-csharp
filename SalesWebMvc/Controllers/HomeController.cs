c4e22646a044ed6d17a68b7aa4a284918e9147a5
Adroaldo <hygino@msn.com>
2021-03-19 16:52:18 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Sales Web mvc app from c# course";
+            ViewData["Aluno"] = "Adroaldo Ferreira";
             return View();
         }
 
